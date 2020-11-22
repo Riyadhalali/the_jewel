6b6d93446f08f5a4fb0a8f9da12a738356322e9f
Riyad <eng.riyadalali93@gmail.com>
2020-11-22 09:50:41 +0200

    Language Activity Selection: 1- Change the Screen Layout accoring to screen size. 2- convert design to responsive design for all phones. 3- added device preview to see all layouts on all screen sizes. 4- add sizer package. 5- change the height and width to be responsive.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,37 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 import 'package:the_jewel/ui/splashscreen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+      DevicePreview(
+        builder: (context) => MyApp(), // Wrap your app
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, // delete deug banner from emulator
-      home: StartScreen(),
+    return LayoutBuilder(
+      //return LayoutBuilder
+      builder: (context, constraints) {
+        return OrientationBuilder(
+          //return OrientationBuilder
+          builder: (context, orientation) {
+            //initialize SizerUtil()
+            SizerUtil().init(constraints, orientation); //initialize SizerUtil
+            return MaterialApp(
+              locale: DevicePreview.locale(context), // Add the locale here
+              builder: DevicePreview.appBuilder, // Add the builder her
+              debugShowCheckedModeBanner: false,
+              title: 'Sizer',
+              theme: ThemeData.light(),
+              home: StartScreen(),
+            );
+          },
+        );
+      },
     );
   }
 }
--- lib/ui/languagescreen.dart
+++ lib/ui/languagescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class LanguageScreen extends StatefulWidget {
   @override
@@ -16,114 +17,104 @@ class _LanguageScreenState extends State<LanguageScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          // background top of screen
-          Container(
-            height: 375.0,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(
-                      'assets/languagescreen/languageactivity_background.png'),
-                  fit: BoxFit.cover),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              child: Image.asset(
+                "assets/languagescreen/languageactivity_background.png",
+                fit: BoxFit.fill,
+              ),
             ),
-          ),
-          SizedBox(
-            height: 50.0,
-          ),
-          //-> Select Language text
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(
-                height: 51.0,
+            SizedBox(
+              height: 3.0.h,
+            ),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  child: Image.asset(
+                    "assets/languagescreen/chooselanguage.png",
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Text(
+                  "Select Language",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 4.0.h,
+            ),
+            GestureDetector(
+              child: Container(
+                alignment: Alignment.center,
+                width: 70.0.w,
+                height: 7.0.h,
                 decoration: BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.contain,
-                    image: AssetImage(
-                      "assets/languagescreen/chooselanguage.png",
+                  borderRadius: BorderRadius.circular(8.0),
+                  gradient: LinearGradient(
+                    begin: Alignment(0.0, -1.0),
+                    end: Alignment(0.0, 1.0),
+                    colors: [const Color(0xff373737), const Color(0xff252424)],
+                    stops: [0.0, 1.0],
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color(0x1a000000),
+                      offset: Offset(0, 10),
+                      blurRadius: 10,
                     ),
-                  ),
+                  ],
+                ),
+                child: Text(
+                  "Arabic",
+                  style: TextStyle(color: Color(0xFFF5AF4B), fontSize: 20.0),
                 ),
               ),
-              Text(
-                "Select Language",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 30.0,
-          ),
-          //Arabic Language Select
-          GestureDetector(
-            child: Container(
-              alignment: Alignment.center,
-              width: 238.0,
-              height: 48.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8.0),
-                gradient: LinearGradient(
-                  begin: Alignment(0.0, -1.0),
-                  end: Alignment(0.0, 1.0),
-                  colors: [const Color(0xff373737), const Color(0xff252424)],
-                  stops: [0.0, 1.0],
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: const Color(0x1a000000),
-                    offset: Offset(0, 10),
-                    blurRadius: 10,
-                  ),
-                ],
-              ),
-              child: Text(
-                "Arabic",
-                style: TextStyle(color: Color(0xFFF5AF4B), fontSize: 20.0),
-              ),
+              onTap: () {
+                print("you clicked my");
+              },
             ),
-            onTap: () {
-              print("you clicked my");
-            },
-          ),
-          SizedBox(
-            height: 30.0,
-          ),
-          //English Language Select
-          GestureDetector(
-            child: Container(
-              alignment: Alignment.center,
-              width: 238.0,
-              height: 48.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8.0),
-                gradient: LinearGradient(
-                  begin: Alignment(0.0, -1.0),
-                  end: Alignment(0.0, 1.0),
-                  colors: [const Color(0xff373737), const Color(0xff252424)],
-                  stops: [0.0, 1.0],
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: const Color(0x1a000000),
-                    offset: Offset(0, 10),
-                    blurRadius: 10,
-                  ),
-                ],
-              ),
-              child: Text(
-                "English",
-                style: TextStyle(color: Color(0xFFF5AF4B), fontSize: 20.0),
-              ),
+            SizedBox(
+              height: 3.0.h,
             ),
-            onTap: () {
-              print("you clicked my");
-            },
-          ),
-        ],
+            GestureDetector(
+              child: Container(
+                alignment: Alignment.center,
+                width: 70.0.w,
+                height: 7.0.h,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  gradient: LinearGradient(
+                    begin: Alignment(0.0, -1.0),
+                    end: Alignment(0.0, 1.0),
+                    colors: [const Color(0xff373737), const Color(0xff252424)],
+                    stops: [0.0, 1.0],
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color(0x1a000000),
+                      offset: Offset(0, 10),
+                      blurRadius: 10,
+                    ),
+                  ],
+                ),
+                child: Text(
+                  "English",
+                  style: TextStyle(color: Color(0xFFF5AF4B), fontSize: 20.0),
+                ),
+              ),
+              onTap: () {
+                print("you clicked my");
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
