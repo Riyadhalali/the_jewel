f5160a9ea8673bb92b33b9d3558460f42108464a
Riyad <eng.riyadalali93@gmail.com>
2020-11-28 21:55:08 +0200

    Select Country Activity has been added

---
+++ lib/ui/selectcountry.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart'; //
+
+class SelectCountry extends StatefulWidget {
+  @override
+  _SelectCountryState createState() => _SelectCountryState();
+}
+
+class _SelectCountryState extends State<SelectCountry> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
