7206a1469a8d21b3b223f601a314a428f70f0498
Riyad <eng.riyadalali93@gmail.com>
2020-12-02 09:40:16 +0200

    Sign in UI: 1- Added background Image. 2- Added Username text view. 3- Added Icon of user 4- added username text input

--- lib/ui/signin.dart
+++ lib/ui/signin.dart
@@ -1,4 +1,7 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:sizer/sizer.dart';
 
 class SignIn extends StatefulWidget {
   @override
@@ -8,6 +11,86 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            SignInImageBackground(),
+            SizedBox(
+              height: 2.0.h,
+            ),
+            UsernameTextView(), // i comment it because i already make it in usernameinput
+            UsernameInput(),
+          ],
+        ),
+      ),
+    );
+  } // end build
+
+//--------------------------Widget Tree----------------------------------------
+  //-> sign in background
+  Widget SignInImageBackground() {
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          child: Image.asset(
+            'assets/signin/signin_background.png',
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          right: 20,
+          child:
+              SignInIcon(), // stacking icon in the botto of the background at the right postion
+        ),
+      ],
+    );
   }
-}
+
+  //-> Widget Sign in user Icon
+  Widget SignInIcon() {
+    return FittedBox(
+      fit: BoxFit.contain,
+      child: Image.asset(
+        'assets/signin/signin_icon.png',
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
+  //-> Text view of username
+
+  Widget UsernameTextView() {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.only(left: 10.0),
+      child: Text(
+        "signin_username".tr().toString(),
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontSize: 16.0.sp,
+          color: Color(0xFF707070),
+        ),
+      ),
+    );
+  }
+
+  //-> Username Input
+
+  Widget UsernameInput() {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.only(left: 10.0),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Enter a search term',
+          labelText: "username",
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+//------------------------------------------------------------------------------
+} // end class
