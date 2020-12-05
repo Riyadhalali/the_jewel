209fef79ad5a39bd3ba1b575ebe735457c1b4ca9
Riyad <eng.riyadalali93@gmail.com>
2020-12-05 10:28:56 +0200

    Main Dart: 1- Added font for different language app. 2- added assets font folder. 3- get the language of application and change the app font based on language in main file in build widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
@@ -33,14 +32,19 @@ class MyApp extends StatelessWidget {
           builder: (context, orientation) {
             //initialize SizerUtil()
             SizerUtil().init(constraints, orientation); //initialize SizerUtil
+            var lang = context.locale.toString(); // get lang of app
+            // print('your app language is:$lang');
             return MaterialApp(
               debugShowCheckedModeBanner: false,
               localizationsDelegates: context.localizationDelegates,
               supportedLocales: context.supportedLocales,
               //locale: DevicePreview.locale(context), // for device preview
               //builder: DevicePreview.appBuilder, // for device preview
-               locale: context.locale,
-              theme: ThemeData.light(),
+              locale: context.locale,
+
+              theme: ThemeData(
+                fontFamily: lang == 'ar' ? 'Ajannat' : 'Roboto',
+              ),
               home: StartScreen(),
             );
           },
--- lib/ui/signin.dart
+++ lib/ui/signin.dart
@@ -232,25 +232,5 @@ class _SignInState extends State<SignIn> {
 //------------------------------------------------------------------------------
 //****************************Functions*****************************************
 
-  //-> Show Dialog
-  showLoaderDialog(BuildContext context) {
-    AlertDialog alert = AlertDialog(
-      content: new Row(
-        children: [
-          CircularProgressIndicator(),
-          Container(
-              margin: EdgeInsets.only(left: 7), child: Text("Loading...")),
-        ],
-      ),
-    );
-    showDialog(
-      barrierDismissible: false,
-      context: context,
-      builder: (BuildContext context) {
-        return alert;
-      },
-    );
-  }
-
 //------------------------------------------------------------------------------
 } // end class
