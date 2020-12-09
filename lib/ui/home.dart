ecb92cd5615b6bccf54b1d3e2bd33c4b52b951cb
Riyad <eng.riyadalali93@gmail.com>
2020-12-09 11:42:49 +0200

    Home Activity: 1- Created bottom navigation Bar. 2- Created Items in Navigation bar.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -6,11 +6,46 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      "Index 0: Home",
+      style: optionStyle,
+    ),
+    Text(
+      "Index 1: Settings",
+      style: optionStyle,
+    ),
+    Text(
+      "Index 2: Profile",
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: CircularProgressIndicator(),
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Business'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Settings')
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber,
+        onTap: _onItemTapped,
       ),
     );
   }
