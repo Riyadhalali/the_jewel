5d5ebfe131f8b82b2993a15e1b81be55b89b0b95
Riyad <eng.riyadalali93@gmail.com>
2020-12-12 16:26:42 +0200

    Home Screen: 1- Show CircleProgressIndicator when fetching image from network

--- lib/components/imageslider.dart
+++ lib/components/imageslider.dart
@@ -41,6 +41,18 @@ class _imageSliderState extends State<imageSlider> {
                 child: Image.network(
                   imgUrl,
                   fit: BoxFit.fill,
+                  loadingBuilder: (BuildContext context, Widget child,
+                      ImageChunkEvent loadingProgress) {
+                    if (loadingProgress == null) return child;
+                    return Center(
+                      child: CircularProgressIndicator(
+                        value: loadingProgress.expectedTotalBytes != null
+                            ? loadingProgress.cumulativeBytesLoaded /
+                                loadingProgress.expectedTotalBytes
+                            : null,
+                      ),
+                    );
+                  },
                 ),
               );
             },
