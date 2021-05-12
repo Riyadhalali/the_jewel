c6a6ae58791a7041f490427236dd384fa4c0d5eb
Riyad <eng.riyadalali93@gmail.com>
2021-05-12 18:00:53 +0300

    Update Sizer package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 import 'package:the_jewel/ui/cart.dart';
 import 'package:the_jewel/ui/countryselect/selectcountry.dart';
 import 'package:the_jewel/ui/home/home.dart';
@@ -23,60 +24,55 @@ import 'package:the_jewel/ui/splashscreen.dart';
       ), // Wrap your app
     );*/
 
-void main() => runApp(
-      EasyLocalization(
-          supportedLocales: [Locale('en'), Locale('ar')],
-          path: 'assets/resources/strings', // <-- change patch to your
-          child: MyApp()),
-    );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+        supportedLocales: [Locale('en'), Locale('ar')],
+        path: 'assets/resources/strings', // <-- change patch to your
+        fallbackLocale: Locale(
+            'en', 'US'), // when call back failed return the english lanuage
+        child: MyApp()),
+  );
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      //return LayoutBuilder
-      builder: (context, constraints) {
-        return OrientationBuilder(
-          //return OrientationBuilder
-          builder: (context, orientation) {
-            //initialize SizerUtil()
-
-            var lang = context.locale.toString(); // get lang of app
-            // print('your app language is:$lang');
-            return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              localizationsDelegates: context.localizationDelegates,
-              supportedLocales: context.supportedLocales,
-              //locale: DevicePreview.locale(context), // for device preview
-              //builder: DevicePreview.appBuilder, // for device preview
-              locale: context.locale,
-              initialRoute: StartScreen.id, // the initial route for the app
-
-              theme: ThemeData(
-                fontFamily: lang == 'ar'
-                    ? 'Ajannat'
-                    : 'Roboto', // select font based on country
-              ),
-              routes: {
-                StartScreen.id: (context) =>
-                    StartScreen(), // creating the routes of the page
-                LanguageScreen.id: (context) => LanguageScreen(),
-                Cart.id: (context) => Cart(),
-                Home.id: (context) => Home(),
-                NavigationBar.id: (context) => NavigationBar(),
-                Notifications.id: (context) => Notifications(),
-                Offers.id: (context) => Offers(),
-                PrivacyPolicy.id: (context) => PrivacyPolicy(),
-                Profile.id: (context) => PrivacyPolicy(),
-                RegisterActivity.id: (context) => RegisterActivity(),
-                SelectCountry.id: (context) => SelectCountry(),
-                SignIn.id: (context) => SignIn(),
-              },
-            );
-          },
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          initialRoute: StartScreen.id,
+          // the initial route for the app
+          theme: ThemeData(
+            fontFamily: context.locale.toString() == 'ar'
+                ? 'Ajannat'
+                : 'Roboto', // select font based on country
+          ),
+          routes: {
+            StartScreen.id: (context) =>
+                StartScreen(), // creating the routes of the page
+            LanguageScreen.id: (context) => LanguageScreen(),
+            Cart.id: (context) => Cart(),
+            Home.id: (context) => Home(),
+            NavigationBar.id: (context) => NavigationBar(),
+            Notifications.id: (context) => Notifications(),
+            Offers.id: (context) => Offers(),
+            PrivacyPolicy.id: (context) => PrivacyPolicy(),
+            Profile.id: (context) => PrivacyPolicy(),
+            RegisterActivity.id: (context) => RegisterActivity(),
+            SelectCountry.id: (context) => SelectCountry(),
+            SignIn.id: (context) => SignIn(),
+          }, // routes
         );
       },
     );
   }
 }
+// var lang=context.locale.toString();
