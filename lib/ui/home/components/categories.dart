f1d1ae6d4e3f482ab4b26f7530442189e6074617
Riyad <eng.riyadalali93@gmail.com>
2021-05-21 18:58:09 +0300

    Home Page: 1- add inkwell to gridview and used flex widget to wrap the content

--- lib/ui/home/components/categories.dart
+++ lib/ui/home/components/categories.dart
@@ -1,7 +1,7 @@
-import 'package:easy_localization/easy_localization.dart';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart';
-import 'package:sizer/sizer.dart';
 
 class Categories extends StatefulWidget {
   static final id = 'categories';
@@ -12,12 +12,6 @@ class Categories extends StatefulWidget {
 class _CategoriesState extends State<Categories> {
   int index = 0;
 
-  List items = [
-    Container(
-      width: 50,
-      height: 50,
-    )
-  ];
   List imgList = [
     'assets/home/categories/makeup.png',
     'assets/home/categories/men_accessories.png',
@@ -36,80 +30,11 @@ class _CategoriesState extends State<Categories> {
 //------------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        //OffersText(),
-        //-> container for categories as listview
-        // Container(
-        //   width: MediaQuery.of(context).size.width * 0.95,
-        //   height: MediaQuery.of(context).size.height * 0.21,
-        //   child: ListView.builder(
-        //       itemCount: imgList.length,
-        //       scrollDirection: Axis.horizontal,
-        //       itemBuilder: (BuildContext context, int index) {
-        //         return Offers(index);
-        //       }),
-        // ),
-        SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-        gridView(),
-      ],
-    );
-  }
-
-//------------------------------------------------------------------------------
-  Widget OffersText() {
-    return Row(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(right: 10.0, left: 10.0),
-          child: Text(
-            "categories".tr().toString(),
-            style: TextStyle(fontSize: 12.0.sp, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Spacer(),
-        TextButton(
-          onPressed: () {
-            //TODO: Go to more details page
-          },
-          child: Text(
-            "load_more".tr().toString(),
-            style: TextStyle(fontSize: 12.0.sp),
-          ),
-        ),
-      ],
-    );
-  }
-
-  //------------------------------Offers----------------------------------------
-  Widget Offers(int index) {
-    return Container(
-      width: 100,
+    return SingleChildScrollView(
       child: Column(
         children: [
-          Container(
-            margin: EdgeInsets.all(5),
-            height: 100,
-            width: 100,
-            decoration: BoxDecoration(
-                // color: Colors.grey.shade100,
-                borderRadius: BorderRadius.circular(10.0),
-                image: DecorationImage(
-                  image: AssetImage(imgList[index].toString()),
-                  fit: BoxFit.fill,
-                )),
-          ),
-          Container(
-            width: 90,
-            child: Column(
-              children: [
-                Text(
-                  "Apple Watch",
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
-            ),
-          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+          gridView(),
         ],
       ),
     );
@@ -121,16 +46,42 @@ class _CategoriesState extends State<Categories> {
       child: GridView.count(
         crossAxisCount: 3,
         childAspectRatio: 1,
-        mainAxisSpacing: 4.0,
-        crossAxisSpacing: 2,
+        mainAxisSpacing: 1.0,
+        crossAxisSpacing: 1,
         shrinkWrap: true, //to hide error message
         children: <Widget>[
-          categoriesImages('assets/home/categories/makeup.svg'),
-          categoriesImages('assets/home/categories/men_accessories.svg'),
-          categoriesImages('assets/home/categories/woman_accessories.svg'),
-          categoriesImages('assets/home/categories/men_perfume.svg'),
-          categoriesImages('assets/home/categories/woman_perfume.svg'),
-          categoriesImages('assets/home/categories/other.svg'),
+          InkWell(
+            child:
+                categoriesImages('assets/home/categories/makeup.svg', 'Makeup'),
+            onTap: () => debugPrint(" make up is pressed "),
+          ),
+          InkWell(
+            child: categoriesImages(
+                'assets/home/categories/men_accessories.svg',
+                'Men Accessories'),
+            onTap: () => debugPrint(" men accessories is pressed "),
+          ),
+          InkWell(
+            child: categoriesImages(
+                'assets/home/categories/woman_accessories.svg',
+                'Woman Accessories'),
+            onTap: () => debugPrint(" woman accessories is pressed "),
+          ),
+          InkWell(
+            child: categoriesImages(
+                'assets/home/categories/men_perfume.svg', 'Men Perfume'),
+            onTap: () => debugPrint(" men perfume is pressed "),
+          ),
+          InkWell(
+            child: categoriesImages(
+                'assets/home/categories/woman_perfume.svg', 'Woman Perfume'),
+            onTap: () => debugPrint(" woman perfume is pressed "),
+          ),
+          InkWell(
+            child:
+                categoriesImages('assets/home/categories/other.svg', 'Other'),
+            onTap: () => debugPrint(" other is pressed "),
+          ),
         ],
       ),
     );
@@ -138,11 +89,31 @@ class _CategoriesState extends State<Categories> {
 
   //----------------------------------------------------------------------------
 
-  Widget categoriesImages(String svg_asset) {
-    return Container(
-      decoration: BoxDecoration(image: DecorationImage(image: Svg(svg_asset))),
+  Widget categoriesImages(String svg_asset, String categorie_name) {
+    return Column(
+      children: [
+        Flexible(
+          flex: 2,
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: Svg(svg_asset), fit: BoxFit.contain)),
+          ),
+        ),
+        Flexible(
+          flex: 1,
+          child: Container(
+            child: Text(
+              categorie_name,
+              maxLines: 2,
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ),
+      ],
     );
   }
 //-----------------------------------------------------------------------------
 }
-//TODO: make it gridview and give it names for every categories using listview and retreive it from api
+//TODO: get the api
