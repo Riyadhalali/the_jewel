e47130d610e4c2c6899b0dee3751396467e97d39
Riyad <eng.riyadalali93@gmail.com>
2021-06-04 14:16:30 +0300

    Added Widget Directory to have all the main widgets like gridview

--- lib/ui/offers.dart
+++ lib/ui/offers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:the_jewel/widgets/gridview.dart';
 
 class Offers extends StatefulWidget {
   static final id = 'offers';
@@ -9,100 +10,24 @@ class Offers extends StatefulWidget {
 class _OffersState extends State<Offers> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white10,
-      body: GridView.count(
-        crossAxisSpacing: 15,
-        mainAxisSpacing: 15,
-        crossAxisCount: 2,
-        children: List.generate(100, (index) => Page()),
-      ),
-    );
-  }
-
-  //-------------------------------------------Widget Tree---------------------
-  Widget Page() {
-    return Center(
-      child: Container(
-        width: MediaQuery.of(context).size.width * 0.4,
-        height: MediaQuery.of(context).size.height * 0.4,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Column(
-          children: [
-            //---------------------------Image------------------------------------
-            Expanded(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Container(
-                  // color: Colors.amber,
-                  width: double.infinity,
-                  constraints: BoxConstraints(
-                      minHeight: MediaQuery.of(context).size.height * 0.1,
-                      maxHeight: MediaQuery.of(context).size.height * 0.25),
-                  child: Image.network(
-                    "https://images.unsplash.com/photo-1502117859338-fd9daa518a9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
-            //----------------------Texts----------------------------------------
-            Container(
-              margin: EdgeInsets.all(5),
-              padding: EdgeInsets.all(5),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 4.0,
-                  ),
-                  Text(
-                    'Description',
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
-                  ),
-                  Text(
-                    '\$ 159.5',
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Quantity: 5',
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.w300),
-                      ),
-                      Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          onTap: () {},
-                          borderRadius: BorderRadius.circular(15),
-                          child: Icon(
-                            Icons.more_horiz,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            )
-          ],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: GridView.count(
+          crossAxisSpacing: 4,
+          mainAxisSpacing: 10,
+          crossAxisCount: 2,
+          childAspectRatio: 2 / 3,
+          shrinkWrap: true, //to hide error message
+          children: List.generate(10, (index) => GridViewWidget()),
         ),
       ),
     );
   }
 }
+
+//-------------------------------------------Widget Tree---------------------
+//   Widget gridView() {}
+// }
 
 //TODO: orgainise code and rename screen to feeds
---
+++ lib/widgets/gridview.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class GridViewWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.45,
+        height: MediaQuery.of(context).size.height * 0.4,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Color(0xFFE0E0E0),
+        ),
+        child: Column(
+          children: [
+            //---------------------------Image------------------------------------
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Container(
+                  // color: Colors.amber,
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  constraints: BoxConstraints(
+                      minHeight: MediaQuery.of(context).size.height * 0.2,
+                      maxHeight: MediaQuery.of(context).size.height * 0.4),
+                  child: Image.network(
+                    "https://images.unsplash.com/photo-1502117859338-fd9daa518a9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            //   ----------------------Texts----------------------------------------
+            Container(
+              margin: EdgeInsets.all(5),
+              padding: EdgeInsets.all(5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 4.0,
+                  ),
+                  Text(
+                    'Description',
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+                  ),
+                  Text(
+                    '\$ 159.5',
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Quantity: 5',
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w300),
+                      ),
+                      Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () {},
+                          borderRadius: BorderRadius.circular(15),
+                          child: Icon(
+                            Icons.more_horiz,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
