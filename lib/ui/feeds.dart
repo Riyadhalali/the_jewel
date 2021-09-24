8fbcc657632712b80dba0c03b76ae2ad52cca2ef
riyadhalali <eng.riyadalali93@gmail.com>
2021-09-24 22:04:19 +0300

    Feeds Screen: 1- added the items as the categorie 2- added the product display in feeds 3- when user select the product it will go to the product image data

--- lib/ui/feeds.dart
+++ lib/ui/feeds.dart
@@ -1,4 +1,9 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:the_jewel/services/showtoast.dart';
+import 'package:the_jewel/webservices/api_calls/webservices.dart';
+import 'package:the_jewel/webservices/models/products/getdataproduct.dart';
 import 'package:the_jewel/widgets/gridview.dart';
 
 class Feeds extends StatefulWidget {
@@ -11,37 +16,69 @@ class Feeds extends StatefulWidget {
 }
 
 class _FeedsState extends State<Feeds> {
+  List<GetdataProduct> getDataProductList = [];
+  ShowToast showToast = new ShowToast();
+
+  //-----------------------------------Get Data Product---------------------------------------
+  Future<List<GetdataProduct>> getDataProduct() async {
+    getDataProductList = await WebServices.getDataProducts(widget.categorieId.toString());
+    return getDataProductList;
+  }
+  //------------------------------------------------------------------------------------------
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color(0xFFE0E0E0),
-        body: GridView.count(
-          crossAxisSpacing: 4,
-          mainAxisSpacing: 10,
-          crossAxisCount: 2,
-          childAspectRatio: 2 / 3,
-          shrinkWrap: true, //to hide error message
-          children: List.generate(10, (index) => GridViewWidget()),
-        ),
-//-> if we want to use staggered GridView
-        // body: StaggeredGridView.countBuilder(
-        //   //padding: 1,
-        //   crossAxisCount: 6,
-        //   itemCount: 8,
-        //   itemBuilder: (BuildContext context, int index) => GridViewWidget(),
-        //   staggeredTileBuilder: (int index) =>
-        //   new StaggeredTile.count(3, index.isEven ? 4 : 5),
-        //   mainAxisSpacing: 8.0,
-        //   crossAxisSpacing: 6.0,
+        // body: GridView.count(
+        //   crossAxisSpacing: 4,
+        //   mainAxisSpacing: 10,
+        //   crossAxisCount: 2,
+        //   childAspectRatio: 2 / 3,
+        //   shrinkWrap: true, //to hide error message
+        //   children: List.generate(10, (index) => GridViewWidget()),
         // ),
+
+//-> if we want to use staggered GridView
+        body: FutureBuilder(
+          future: getDataProduct(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+
+              default:
+                if (snapshot.hasError)
+                  showToast.showToast(snapshot.error.toString());
+                else
+                  return getDataProductList.isEmpty
+                      ? Center(child: Text("sorry no data available".tr().toString()))
+                      : StaggeredGridView.countBuilder(
+                          //padding: 1,
+                          crossAxisCount: 6,
+                          itemCount: getDataProductList.length,
+                          itemBuilder: (BuildContext context, int index) => GridViewWidget(
+                            productId: getDataProductList[index].productId.toString(),
+                            productName: getDataProductList[index].productName.toString(),
+                            productDescription: getDataProductList[index].descr.toString(),
+                            price: getDataProductList[index].price.toString(),
+                            image: getDataProductList[index].picture.toString(),
+                          ),
+                          staggeredTileBuilder: (int index) =>
+                              new StaggeredTile.count(3, index.isEven ? 4 : 5),
+                          mainAxisSpacing: 8.0,
+                          crossAxisSpacing: 6.0,
+                        );
+                //-> in case there is  data and there is no error but the future complete with no data and there is no error
+                return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
       ),
     );
   }
 }
-
-//-------------------------------------------Widget Tree---------------------
-//   Widget gridView() {}
-// }
-
-//TODO: orgainise code and rename screen to feeds
+//--------------------------------------------------------------------------------------
--- lib/widgets/gridview.dart
+++ lib/widgets/gridview.dart
@@ -1,88 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:the_jewel/ui/product_details/product_details.dart';
 
 class GridViewWidget extends StatelessWidget {
+  String productId = '';
+  String productName = '';
+  String productDescription = '';
+  String price = '';
+  String image = '';
+  GridViewWidget(
+      {required this.productId,
+      required this.productName,
+      required this.productDescription,
+      required this.price,
+      required this.image});
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: MediaQuery.of(context).size.width * 0.45,
-        height: MediaQuery.of(context).size.height * 0.4,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          color: Colors.white,
-        ),
-        child: Column(
-          children: [
-            //---------------------------Image------------------------------------
-            Expanded(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Container(
-                  width: double.infinity,
-                  height: MediaQuery.of(context).size.height * 0.2,
-                  constraints: BoxConstraints(
-                      minHeight: MediaQuery.of(context).size.height * 0.2,
-                      maxHeight: MediaQuery.of(context).size.height * 0.4),
-                  child: Image.network(
-                    "https://images.unsplash.com/photo-1502117859338-fd9daa518a9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                    fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {
+        //-> go to page details
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => ProductDetails(
+              productId: productId,
+              productName: productName,
+              price: price,
+              description: productDescription,
+              picture: image,
+            ),
+          ),
+        );
+      },
+      child: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.45,
+          height: MediaQuery.of(context).size.height * 0.4,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.white,
+          ),
+          child: Column(
+            children: [
+              //---------------------------Image------------------------------------
+              Expanded(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Container(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height * 0.2,
+                    constraints: BoxConstraints(
+                        minHeight: MediaQuery.of(context).size.height * 0.2,
+                        maxHeight: MediaQuery.of(context).size.height * 0.4),
+                    child: Image.network(
+                      image,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
-            ),
-            //   ----------------------Texts----------------------------------------
-            Container(
-              margin: EdgeInsets.all(5),
-              padding: EdgeInsets.all(5),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 4.0,
-                  ),
-                  Text(
-                    'Description',
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
-                  ),
-                  Text(
-                    '\$ 159.5',
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Quantity: 5',
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.w300),
-                      ),
-                      Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          onTap: () {},
-                          borderRadius: BorderRadius.circular(15),
-                          child: Icon(
-                            Icons.more_horiz,
-                            color: Colors.grey,
+              //   ----------------------Texts----------------------------------------
+              Container(
+                margin: EdgeInsets.all(5),
+                padding: EdgeInsets.all(5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 4.0,
+                    ),
+                    Text(
+                      productName,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      '\$${price.toString()}',
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style:
+                          TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600, color: Colors.red),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            productDescription,
+                            overflow: TextOverflow.clip,
+                            maxLines: 1,
+                            style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
                           ),
                         ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            )
-          ],
+                        Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            onTap: () {},
+                            borderRadius: BorderRadius.circular(15),
+                            child: Icon(
+                              Icons.more_horiz,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
+//TODO: add the three points in gridview
